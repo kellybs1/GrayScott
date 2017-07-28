7a72827c9e7553c6a91f8042ebba76b2aa5f0808
kellybs1 <kellybs1@student.op.ac.nz>
2017-07-28 21:29:42 +1200

    disabled windows maximise

--- ReactionAssignment/Form1.Designer.cs
+++ ReactionAssignment/Form1.Designer.cs
@@ -516,6 +516,7 @@
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Gray-Scott Simulation";
             this.Load += new System.EventHandler(this.Form1_Load);
